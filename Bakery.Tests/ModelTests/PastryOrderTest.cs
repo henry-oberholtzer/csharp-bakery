90ecfa51cbc766a91483e6c30fb9b2ed57189664
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-19 09:54:07 -0800

    pastry order passes second test

--- Bakery.Tests/ModelTests/PastryOrderTest.cs
+++ Bakery.Tests/ModelTests/PastryOrderTest.cs
@@ -9,5 +9,13 @@ namespace Bakery.Models
             PastryOrder order = new(2);
             Assert.AreEqual(order.GetType(), typeof(PastryOrder));
         }
+
+        [TestMethod]
+        public void PastryOrderConstructor_GetQuantity_int()
+        {
+            int expected = 3;
+            PastryOrder order = new(expected);
+            Assert.AreEqual(order.Quantity, expected);
+        }
     }
 }
